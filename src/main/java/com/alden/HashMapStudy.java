e8132e7bd5a2370ae1e334ed7493e209e7a87c36
will-null <suiai1994@163.com>
2020-09-14 17:46:10 +0800

    ade

--- src/main/java/com/alden/HashMapStudy.java
+++ src/main/java/com/alden/HashMapStudy.java
@@ -3,7 +3,7 @@ package com.alden;
 import org.junit.Test;
 
 import java.util.HashMap;
-import java.util.HashSet;
+
 
 public class HashMapStudy {
 
@@ -11,13 +11,20 @@ public class HashMapStudy {
     public void hashStudy() {
         HashMap<String, String> hh = new HashMap<String, String>();
         hh.put("姓名", "周瑜2");
-        String now = hh.put("1","3");
+        String now = hh.put("姓名","3");
         System.out.println(now);
-
+        String now1 = hh.put("姓名","33");
+        System.out.println(now1);
     }
 
     @Test
     public void wei(){
+        int x=1;
+        System.out.println(x++);      //1
+        System.out.println(++x);      //2
+
+        System.out.println("==================================================================================");
+
         /* 8*2的-2次方 ==> 8*(1/4)*/
         System.out.println(8>>>2);
         System.out.println("==================================================================================");
